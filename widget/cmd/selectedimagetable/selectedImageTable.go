bee3b9a468e934a0092293c859dfa1f65774bc77
Jerome Le Saux <jerome.lesaux@treezor.com>
2023-08-16 15:45:08 +0200

    add: add selected image table widget

---
+++ widget/cmd/selectedimagetable/selectedImageTable.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+	ui "github.com/jeromelesaux/fyne-io/widget"
+)
+
+var size float32 = 100.
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("image table")
+	w.Resize(fyne.NewSize(400, 400))
+	imgs := []image.Image{OpenImage(), OpenImage(), OpenImage()}
+	t := ui.NewImageSelectionTableWithImages(imgs, fyne.NewSize(size, size))
+	w.SetContent(
+		container.New(
+			layout.NewAdaptiveGridLayout(2),
+			container.New(layout.NewGridLayoutWithRows(3),
+				container.New(
+					layout.NewAdaptiveGridLayout(1),
+					widget.NewButton("append", func() {
+						t.Append(canvas.NewImageFromImage(OpenFyne()))
+					}),
+				),
+				container.New(
+					layout.NewAdaptiveGridLayout(1),
+					widget.NewButton("count", func() {
+						fmt.Println("nb selected images", len(t.Images()))
+					}),
+				),
+			),
+
+			container.NewScroll(
+				t,
+			),
+		),
+	)
+
+	w.ShowAndRun()
+}
+
+func OpenFyne() image.Image {
+	f, err := os.Open("fyne.png")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	img, err := png.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+	return img
+}
+
+func OpenImage() image.Image {
+	f, err := os.Open("image.png")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	img, err := png.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+	return img
+}
---
+++ widget/selected_image_table.go
@@ -0,0 +1,94 @@
+package custom_widget
+
+import (
+	"image"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+)
+
+type ImageSelectionTable struct {
+	widget.Table
+	images   []*canvas.Image
+	selected []bool
+	size     fyne.Size
+}
+
+func NewImageSelectionTableWithImages(imgs []image.Image, size fyne.Size) *ImageSelectionTable {
+	table := &ImageSelectionTable{
+		images:   make([]*canvas.Image, len(imgs)),
+		selected: make([]bool, len(imgs)),
+		size:     size,
+	}
+
+	for i := 0; i < len(imgs); i++ {
+		table.selected[i] = true
+		table.images[i] = canvas.NewImageFromImage(imgs[i])
+	}
+
+	table.UpdateCell = table.updateCell
+	table.Length = table.length
+	table.CreateCell = table.createCell
+	table.OnSelected = table.onSelect
+	table.ExtendBaseWidget(table)
+
+	return table
+}
+
+func (t *ImageSelectionTable) Images() []image.Image {
+	im := make([]image.Image, 0)
+	for i := 0; i < len(t.images); i++ {
+		if t.selected[i] {
+			im = append(im, t.images[i].Image)
+		}
+	}
+	return im
+}
+
+func (t *ImageSelectionTable) onSelect(id widget.TableCellID) {
+	t.selected[id.Col] = !t.selected[id.Col]
+	t.Refresh()
+	canvas.Refresh(t)
+}
+
+func (t *ImageSelectionTable) Append(img *canvas.Image) {
+	t.images = append(t.images, img)
+	t.selected = append(t.selected, true)
+	t.Refresh()
+	canvas.Refresh(t)
+}
+
+func (t *ImageSelectionTable) createCell() fyne.CanvasObject {
+	c := container.NewMax(emptyCell(t.size), widget.NewIcon(nil))
+	//	c.Resize(fyne.NewSize(t.size.Width, t.size.Height*2))
+	return c
+}
+
+func (t *ImageSelectionTable) updateCell(id widget.TableCellID, o fyne.CanvasObject) {
+	i := o.(*fyne.Container).Objects[0].(*canvas.Image)
+	c := o.(*fyne.Container).Objects[1].(*widget.Icon)
+
+	switch id.Row {
+	case 0:
+		c.Hide()
+		i.Image = t.images[id.Col].Image
+		i.SetMinSize(t.size)
+		i.Refresh()
+		i.Show()
+	case 1:
+		if t.selected[id.Col] {
+			c.SetResource(theme.ConfirmIcon())
+		} else {
+			c.SetResource(theme.CancelIcon())
+		}
+		c.Show()
+	}
+
+}
+
+func (t *ImageSelectionTable) length() (int, int) {
+	return 2, len(t.images)
+}
